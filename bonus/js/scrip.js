fc67a496bd430604aa088d38ea9dc317a96a7ac3
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-25 15:06:21 +0100

    Uploade dice game

--- bonus/js/scrip.js
+++ bonus/js/scrip.js
@@ -1,10 +1,9 @@
-
 let email_list = ['ciccio@gmail.com', 'franco@gmail.com', 
 'pippo@gmail.com', 'giovanni@gmail.com'];
 
-let button = document.getElementById('send');
+let btn_email = document.getElementById('send');
 
-button.addEventListener('click', function(){
+btn_email.addEventListener('click', function(){
 
     let email = document.getElementById('email').value;
     let message = document.getElementById('message');
@@ -17,10 +16,33 @@ button.addEventListener('click', function(){
     }
     
     if(flag == true){
-        document.getElementById('message').innerHTML = 'Email inserita correttamente. Ora puoi tirare i dadi!'
+        document.getElementById('message').innerHTML = 'Email inserita correttamente. Ora puoi tirare i dadi!';
+        
+        let btn_dadi = document.getElementById('btn_dadi');
+
+        btn_dadi.style.display = 'block';
+
+        btn_dadi.addEventListener('click', function(){
+            let giocatore = Math.round(Math.random()*5 + 1);
+            let computer = Math.round(Math.random()*5 + 1);
+
+            document.getElementById('player').innerHTML = 'Il tuo dado : ' + giocatore;
+            document.getElementById('computer').innerHTML = 'Computer : ' + computer;
+
+            if(giocatore > computer){
+                document.getElementById('win').innerText = 'Complimenti! HAI VINTO!';
+            }
+            else if(giocatore == computer){
+                document.getElementById('x').innerText = 'PAREGGIO! Riprova';
+            }
+            else{
+                document.getElementById('defeat').innerText = 'Oh no...hai perso...prova a ribattere il computer!';
+            }
+            
+        })
     }
     else{
-        document.getElementById('message').innerHTML = 'Mi dispiace, l\'email che hai inserito non è corretta. Riprova...'
+        document.getElementById('message').innerHTML = 'Mi dispiace, l\'email che hai inserito non è corretta. Riprova...';
     }
 })
 
